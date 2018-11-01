af2a50fb2e663a45e1b6e816e4fc6aacf7179af3
Jamin Peacock <knightjam7@yahoo.com>
2018-11-01 14:57:01 -0600

    first commit

---
+++ run.sh
@@ -0,0 +1,27 @@
+export s="<html>
+<head>
+
+</head>
+<body>
+
+<h1> Hello! </h1>
+<div>
+<a href= "file:///home/jamin/A.html"> Go to A!</a>
+</div>
+
+<div>
+
+</div>
+
+</body>
+</html>"
+export a="<html>
+<head>
+
+</head>
+<body>
+<h1> Hello 2! </h1>
+</body>
+</html>"
+echo $s > index.html
+echo $a > A.html
